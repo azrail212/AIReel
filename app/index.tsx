8842ddc50a055527b2261d060de7de401a1ba944
azrail <azraa.kadric@gmail.com>
2025-11-05 10:35:24 +0100

    Initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,20 @@
 import { Stack, Link } from 'expo-router';
 
-import { View } from 'react-native';
+import { View,Text} from 'react-native';
 
 import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
 import { ScreenContent } from '@/components/ScreenContent';
 
+import {StatusBar} from 'expo-status-bar'
+
 export default function Home() {
   return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </View>
+    <View className="flex-1 items-center justify-center bg-white">
+          <Text className="text-3xl font-pblack">AIReel</Text>
+          <StatusBar style="auto"></StatusBar>
+          <Link href="/profile" style={{color:'blue'}}>Go to Profile</Link>
+        </View>
   );
 }
 
-const styles = {
-  container: 'flex flex-1 bg-white',
-};
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,8 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// Learn more: https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-
 const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: './global.css' });
