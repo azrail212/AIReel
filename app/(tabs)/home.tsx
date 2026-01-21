c3e54c284d8d45809d066439380a7a91345ecfb0
azrail <azraa.kadric@gmail.com>
2026-01-21 15:01:07 +0100

    Cleanup+Minor work on showing dynamic data

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -8,6 +8,7 @@ import EmptyState from '@/components/EmptyState'
 import { getAllPosts, getLatestVideos } from '@/lib/appwrite'
 import useAppwrite from '@/lib/useAppwrite'
 import VideoCard from '@/components/VideoCard'
+import { useGlobalContext } from '@/context/GlobalProvider'
 
 const Home = () => {
   const { data: posts, refetch} = useAppwrite(getAllPosts);
@@ -15,6 +16,8 @@ const Home = () => {
   const { data: latestVideos} = useAppwrite(getLatestVideos);
 
   const [refreshing, setRefreshing] = useState(false)
+
+   const { user, setUser, setIsLoggedIn } = useGlobalContext();
 
   const onRefresh = async () => {
     setRefreshing(true);
@@ -38,10 +41,10 @@ const Home = () => {
           <View className='justify-between items-start flex-row mb-6'>
             <View>
               <Text className='font-pmedium text-sm text-gray-100'>
-                Welcome Back
+                Welcome Back,
               </Text>
               <Text className='font-psemibold text-2xl text-white'>
-                Azra
+                {user?.username}
               </Text>
             </View>
             <View className='mt-1.5'>
